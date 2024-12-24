cac6e34d0c5147a6ba73bd363496a821091a9dc3
LambTheBamb <103245638+LambTheBamb@users.noreply.github.com>
2024-12-24 16:26:39 +0500

    Create redis-ignore.sh
    Was used for testing

---
+++ redis-ignore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /home/master/applications
+
+for A in $(ls | awk '{print $NF}'); do
+    echo "Processing application: $A"
+    cd /home/master/applications/$A/public_html/ || { echo "Failed to change directory to $A/public_html"; continue; }
+
+    if wp redis info --allow-root; then
+        status="Enabled"
+        echo "$A: $status"
+    else
+        status="Failed"
+        echo "$A: $status"
+        curl -X POST -H "Content-Type: application/json" -d '{"ip":"'"$(curl -s ifconfig.me)"'","application":"'"$A"'","status":"'"$status"'"}' https://phpstack-1246017-4482242.cloudwaysapps.com/disabled
+    fi
+
+    # Log the result to the API
+#    curl -X POST -H "Content-Type: application/json" -d '{"ip":"'"$(curl -s ifconfig.me)"'","application":"'"$A"'","status":"'"$status"'"}' https://phpstack-1246017-4482242.cloudwaysapps.com/applications
+    cd /home/master/applications || { echo "Failed to change back to /home/master/applications"; exit 1; }
+done
